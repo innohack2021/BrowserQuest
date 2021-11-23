9c2dc63fcd0add9ec8e6c12f3d77b6b0660b06f4
tmzpdpdlwl <tmzpdpdlwl@gmail.com>
2021-11-23 03:19:37 +0000

    refactor: Refactor seperate server and client

--- bin/start_dev_client.js
+++ bin/start_dev_client.js
@@ -1,12 +1,14 @@
 var connect = require('connect');
+var logger = require('morgan');
+var serverStatic = require('serve-static')
 var app = connect();
 var port = 8080;
 
 // Display incoming requests, coloured for status
-app.use(connect.logger('dev'));
+app.use(logger('dev'));
 
 // Serve everything in the client subdir statically
-app.use(connect.static('client'));
+app.use(serverStatic('client'));
 
 // This lets us return and log 404's, which is
 // very useful for debugging configuration problems
