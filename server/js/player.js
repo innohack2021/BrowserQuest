de5ecc270b160369c34246428542f8a1d0ef5e20
tmzpdpdlwl <tmzpdpdlwl@gmail.com>
2021-11-29 05:49:45 +0900

    fix: Fix count init

--- server/js/player.js
+++ server/js/player.js
@@ -326,7 +326,8 @@ module.exports = Player = Character.extend({
             for (i = 0; i < items.length; i++) {
               strarry = items[i].split("num:");
               if (strarry[0] == "x:" + x + "y:" + y) {
-                count = parseInt(strarry[1]) + 1 - 5; //어거지로 4뺀다.
+                count = 0;
+                // parseInt(strarry[1]) + 1 ;
               }
             }
             if (count != 0) t_count = count;
