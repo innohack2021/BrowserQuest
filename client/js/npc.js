dd7b199ae0d765f951b6c01a42f94480a5bd1621
tmzpdpdlwl <tmzpdpdlwl@gmail.com>
2021-11-29 01:03:31 +0900

    fix: Fix server config

--- client/js/npc.js
+++ client/js/npc.js
@@ -1,33 +1,33 @@
-define(['require','character'], function(require, Character) {
-    var Npc = Character.extend({
-        init: function(id, kind) {
-            this._super(id, kind, 1);
-            this.itemKind = Types.getKindAsString(this.kind);
-        },
-        talk: async function(description) {
-            const chatUrl = "http://localhost:8081/clovaChat";
-            var script = "hi stranger";
-            try {
-                const fetchInit = {
-                    method: "POST",
-                    headers: {
-                    "Content-Type": "application/json",
-                    },
-                    body: JSON.stringify({
-                        description,
-                        npc: this.itemKind
-                    }),
-                }
-                script = await fetch(chatUrl, fetchInit)
-                                    .then(async (response) => await response.json())
-                                    .catch((error) => console.log("error:", error));
-                script = script.chat
-            } catch(e) {
-                console.log('💔', e)
-                // Exception triggered when config_local.json does not exist. Nothing to do here.
-            }
-            return script;
-        }
-    });
-    return Npc;
+define(["require", "character"], function (require, Character) {
+  var Npc = Character.extend({
+    init: function (id, kind) {
+      this._super(id, kind, 1);
+      this.itemKind = Types.getKindAsString(this.kind);
+    },
+    talk: async function (description) {
+      const chatUrl = "http://menthangverse.42seoul.k:8081/clovaChat";
+      var script = "hi stranger";
+      try {
+        const fetchInit = {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            description,
+            npc: this.itemKind,
+          }),
+        };
+        script = await fetch(chatUrl, fetchInit)
+          .then(async (response) => await response.json())
+          .catch((error) => console.log("error:", error));
+        script = script.chat;
+      } catch (e) {
+        console.log("💔", e);
+        // Exception triggered when config_local.json does not exist. Nothing to do here.
+      }
+      return script;
+    },
+  });
+  return Npc;
 });
